aeecc07a77b594aaec39dfae5988bd11a76d2389
batalamast <batalamast@gmail.com>
2023-01-24 13:23:06 +0200

    update admin product categories

--- controllers/admin/AdminProductCategoriesController.js
+++ controllers/admin/AdminProductCategoriesController.js
@@ -18,27 +18,19 @@ const adminProductCategoriesController = {
     updateProductCategories: async (req, res) => {
         const paramId = req.params._id;
         const category = await ProductCategory.findById(paramId);
-        let title;
         let subCategories;
-        let slug;
-
-        if (req.body.title) {
-            title = req.body.title;
-        }
-        if (req.body.slug) {
-            slug = req.body.slug;
-        }
 
         if (req.body.subCategories) {
             subCategories = category.subCategories.concat(req.body.subCategories);
         } else {
             subCategories = category.subCategories;
         }
-        
+
         const dataToUpdate = {
-            title: title,
+            title: req.body.title && req.body.title,
+            slug: req.body.slug && req.body.slug,
             subCategories: subCategories,
-            slug: slug
+            modelRef: req.body.modelRef && req.body.modelRef,
         }
 
         const update = await ProductCategory.updateOne({_id: paramId}, dataToUpdate);
