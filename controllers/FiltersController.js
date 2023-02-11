6d6b14a8895797fbc464807c2c1b32b5dd3fb432
ArxLancifer <a_christoforidis@hotmail.com>
2023-02-12 00:24:56 +0200

    fetch products by species

--- controllers/FiltersController.js
+++ controllers/FiltersController.js
@@ -1,4 +1,6 @@
+const AlcoholDrinkModel = require("../models/AlcoholDrinkModel");
 const ProductCategory = require("../models/ProductsCategory");
+const ProductSubCategory = require("../models/ProductSubCategory");
 
 const FiltersController = {
     fetchCategoriesForFilters: async (req, res) => {
@@ -17,6 +19,33 @@ const FiltersController = {
             console.error(e);
         }
     },
+    fetchFilteredProducts: async (req, res)=> {
+        try {
+            const filteredCategories = await ProductSubCategory
+                .find({slug:req.body})
+                .select("slug category")
+                .populate([
+                    {
+                        path:"category",
+                        select: "modelRef slug"
+                    }
+                ])
+                // const dynamicModel = require(`../models/${filteredCategories[0].category.modelRef}`)
+                const dynamicModel = require("../models/" + filteredCategories[0].category.modelRef);
+                const collectionName = dynamicModel.collection.collectionName;
+                
+            const subCategoriesIds = filteredCategories.map(option =>{
+                return option._id;
+            })
+
+            const filteredItems = await dynamicModel.find({subCategory:subCategoriesIds});
+            console.log(filteredCategories[0].category.modelRef)
+            res.json({filteredItems});
+        } catch (error) {
+            res.send(error);
+        }
+    }
+
 }
 
 module.exports = FiltersController;
--- routes/filters.js
+++ routes/filters.js
@@ -2,5 +2,5 @@ const route = require("express").Router();
 const FiltersController = require("../controllers/FiltersController");
 
 route.get("/categories/:slug", FiltersController.fetchCategoriesForFilters);
-
+route.post("/sub-categories", FiltersController.fetchFilteredProducts);
 module.exports = route;
