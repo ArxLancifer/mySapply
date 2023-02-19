1e719643aa207c8786a25dce5d28691552a0a784
ArxLancifer <a_christoforidis@hotmail.com>
2023-02-19 22:40:10 +0200

    New getProductByType route

--- controllers/ProductSubCategories.js
+++ controllers/ProductSubCategories.js
@@ -13,6 +13,34 @@ const ProductCategories = {
 
         return res.json(productSubCategory);
     },
+    getAllProductsByType: async function (req, res) {
+        try {
+            const paramSlug = req.params.slug;
+            if (!paramSlug) {
+                return res.status(400).json({message: "paramSlug not found"});
+            }
+
+            const subCategory = await ProductSubCategory
+                .findOne({slug: paramSlug})
+                .populate([
+                    {
+                        path: "category",
+                        select: "modelRef subCategories",
+                    }
+                ]);
+
+            if (!subCategory.category.modelRef) {
+                return res.status(400).json({message: "modelRed not found"});
+            }
+            
+            const dynamicModelCollection = require(`../models/${subCategory.category.modelRef}`);
+            const allProducts = await dynamicModelCollection.find({subCategory:subCategory._id}).
+                    populate(["subCategory"]);
+            return res.json(allProducts);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 }
 
 module.exports = ProductCategories;
